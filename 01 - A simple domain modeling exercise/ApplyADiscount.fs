0efa1637d7b41fff2fccb4888ca04da2680ac4e0
Paul Watts <7057417+PaulFWatts@users.noreply.github.com>
2021-09-18 05:10:08 +1000

    Completed 01 - A simple domain modelling exercise

---
+++ 01 - A simple domain modeling exercise/ApplyADiscount.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type RegisteredCustomer = {Id : string}
+type UnregisteredCustomer = {Id : string}
+
+type Customer = 
+    | EligibleRegistered of RegisteredCustomer
+    | Registered of RegisteredCustomer
+    | Guest of UnregisteredCustomer
+
+let calulateTotal customer spend =
+    let discount =
+        match customer with
+         | EligibleRegistered _ when spend >= 100.0M -> spend * 0.1M
+         | _ -> 0.0M  
+    spend - discount
+
+let john = EligibleRegistered { Id = "John"}
+let mary = EligibleRegistered { Id = "Mary"}
+let richard = Registered { Id = "Richard"}
+let sarah = Guest { Id = "Sarah"}
+
+let assertJohn = calulateTotal john 100.0M = 90.0M
+let assertMary = calulateTotal mary 99.0M = 99.0M
+let assertRichard = calulateTotal richard 100.0M = 100.0M
+let assertSarah = calulateTotal sarah 100.0M = 100.0M
+
+
+
